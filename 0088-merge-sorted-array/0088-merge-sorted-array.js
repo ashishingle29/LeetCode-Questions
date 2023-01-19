6f3569e0f1dd46870517455409fb63b13ac90394
ASHISH INGLE <92534028+ashishingle29@users.noreply.github.com>
2023-01-20 01:11:46 +0530

    Time: 65 ms (75.71%), Space: 41.9 MB (79.28%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -6,55 +6,35 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 
-
-
-
-
-
-
 var merge = function(nums1, m, nums2, n) {
 //     let p = m-1
 //     let q = n-1
 //     let r = n+m-1
     
-//     for(let i=0; i<r; i++){ //n
-//         if(nums1[p]<=nums2[q]){ 
-//             nums1[r]=nums2[q]  
-//             r--;
-//             q--;
-//         }else{
-//             nums1[r]=nums1[p];
-//             p--;
-//             r--;
-//             if(nums1[p]<=nums2[q]){
-//                 nums1[r]=nums1[p];
-//             }else{
-//                 if(nums2[q]<nums1[p]){
-//                     nums1[r]= nums1[p]
-//                 }
-//             }
+//     for(let i=r; i<=0; i--){ 
+//         if(p<0){nums1[i]=nums2[q--]}else if(q<0){nums1[i]=nums1[p--]}
+//         else if(nums1[p]<=nums2[q]){ 
+//             nums1[i]=nums2[q--]  
+//         }else if(nums2[q]<nums1[p]) {
+//             nums1[i]=nums1[p--];
 //         }
 //     }
 //     return nums1;
     
-     let p = m+n-1;
-        m = m-1;
-        n = n-1;
-        
-        while(n>=0){
-            
-            if(m>=0 && nums1[m]> nums2[n])
-            {
-                nums1[p] = nums1[m];
-                p--;
-                m--;
-            }
-            else
-            {
-                nums1[p] = nums2[n];
-                p--;
-                n--;
-            }
+    let p = m-1;
+    let q = n-1;
+    let r = m+n-1;
+    
+    for(let i=r; i>=0; i--){ //n
+         if(p<0){
+             nums1[i] = nums2[q--];
+         }else if(q<0){
+             nums1[i] = nums1[p--];
+         }else if(nums1[p]<=nums2[q]){
+                nums1[i]=nums2[q--];
+            }else  if((nums2[q]<nums1[p])){
+                    nums1[i]= nums1[p--]
+                
         }
-    return nums1;
     }
+};
