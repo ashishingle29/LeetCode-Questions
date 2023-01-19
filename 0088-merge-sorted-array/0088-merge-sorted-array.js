ccebc111b49cbedd8dfa75f869266d4aead24bab
ASHISH INGLE <92534028+ashishingle29@users.noreply.github.com>
2023-01-20 01:23:11 +0530

    Time: 60 ms (90.47%), Space: 42.3 MB (37.78%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -6,21 +6,7 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 
-var merge = function(nums1, m, nums2, n) {
-//     let p = m-1
-//     let q = n-1
-//     let r = n+m-1
-    
-//     for(let i=r; i<=0; i--){ 
-//         if(p<0){nums1[i]=nums2[q--]}else if(q<0){nums1[i]=nums1[p--]}
-//         else if(nums1[p]<=nums2[q]){ 
-//             nums1[i]=nums2[q--]  
-//         }else if(nums2[q]<nums1[p]) {
-//             nums1[i]=nums1[p--];
-//         }
-//     }
-//     return nums1;
-    
+var merge = function(nums1, m, nums2, n) {    
     let p = m-1;
     let q = n-1;
     let r = m+n-1;
