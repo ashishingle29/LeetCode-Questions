fd8ff27fcc587598c9c33bc39fd58fe0b3418078
ASHISH INGLE <92534028+ashishingle29@users.noreply.github.com>
2023-02-01 01:24:44 +0530

    Time: 55 ms (99.87%), Space: 44.4 MB (64.79%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function(nums) {
+    
+   let total=0;
+    
+    for(let i=0; i<nums.length; i++){
+        total= total + nums[i]
+    }
+   
+     let leftsum=0;  
+    let rightsum=0;  
+   
+    
+    for(let j=0; j<nums.length; j++){
+
+       
+        rightsum = total - leftsum - nums[j]  //27
+
+        if(leftsum==rightsum){
+            return j
+        }
+        leftsum = leftsum + nums[j]   
+    }
+    return -1
+    
+};
