b0c8db15670dca617ace9cab2be4fbae5a4b4ac8
ASHISH INGLE <92534028+ashishingle29@users.noreply.github.com>
2023-02-04 02:53:42 +0530

    Time: 72 ms (89.11%), Space: 48.4 MB (49.98%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.js
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.js
@@ -3,21 +3,27 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-    let str = s.split(" ")
-     let newarr =[]
-    for(let i=0; i<str.length; i++){
-        let sstr = str[i].split("")
-        let l=0
-        let r = sstr.length-1
+//     let str = s.split(" ")
+//      let newarr =[]
+//     for(let i=0; i<str.length; i++){
+//         let sstr = str[i].split("")
+//         let l=0
+//         let r = sstr.length-1
        
-        while(l<r){
-            sstr[l]= sstr[l].concat(sstr[r])
-            sstr[r]= sstr[l].slice(0,-1)
-            sstr[l]= sstr[l].slice(1)
-            l++
-            r--
-        }
-        newarr.push(sstr.join(""))
-    }
-    return newarr.join(" ")
+//         while(l<r){
+//             sstr[l]= sstr[l].concat(sstr[r])
+//             sstr[r]= sstr[l].slice(0,-1)
+//             sstr[l]= sstr[l].slice(1)
+//             l++;
+//             r--;
+//         }
+//         newarr.push(sstr.join(""))
+//     }
+//     return newarr.join(" ")
+    
+    return s.split(" ").map(function(word) {
+        return word.split("").reverse().join("");
+    }).join(" ");
+    
+    
 };
