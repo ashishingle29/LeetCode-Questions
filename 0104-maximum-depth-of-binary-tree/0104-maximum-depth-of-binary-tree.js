401c97c3c1556d76b8f31589f40a992ae77cc29a
ASHISH INGLE <92534028+ashishingle29@users.noreply.github.com>
2023-02-20 18:08:25 +0530

    Time: 68 ms (80.04%), Space: 45 MB (30.19%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    if(root==null) {
+        return 0
+    }
+    if(root.left==null && root.right==null) {
+        return 1
+    }
+    
+    let hleft = maxDepth(root.left);
+    let hright = maxDepth(root.right);
+    
+    let maxhight = Math.max(hleft,hright)+1;
+    
+    return maxhight;
+};
