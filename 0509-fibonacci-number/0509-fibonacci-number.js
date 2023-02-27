01198eabf8c459e59e0cd60482484cde62444b12
ASHISH INGLE <92534028+ashishingle29@users.noreply.github.com>
2023-02-27 14:44:26 +0530

    Time: 62 ms (81.29%), Space: 42.1 MB (21.62%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -3,9 +3,21 @@
  * @return {number}
  */
 var fib = function(n) {
-    if(n === 0) return 0
-  	if(n === 1) return 1
+    //Method 1
     
-   return  fib(n - 1) + fib(n - 2)
+    let fiba = [0,1]
+    
+    for(let i=2; i<=n; i++){
+        let x = fiba[i-1]+ fiba[i-2]
+        fiba.push(x)
+    }
+    
+   return fiba[n] 
+    
+    
+    //Method 2
+//     if(n === 0) return 0;
+//   	if(n === 1) return 1;
+//     return  fib(n - 1) + fib(n - 2)
     
 };
