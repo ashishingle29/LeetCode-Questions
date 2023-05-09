459cd98050a365d07934d93243f0e2f314dd5d69
ASHISH INGLE <92534028+ashishingle29@users.noreply.github.com>
2023-05-09 13:07:05 +0530

    Time: 58 ms (63.01%), Space: 42.2 MB (34.54%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -2,11 +2,26 @@
  * @param {number[]} nums
  * @return {number}
  */
+// var findMin = function(nums) {
+//     // Brute force O(N)
+//     let currentMin = nums[0];
+//     for(let i = 1; i < nums.length; i++) {
+//       currentMin = Math.min(currentMin, nums[i]);
+//     }
+//   return currentMin;
+// };
+
+
 var findMin = function(nums) {
-    // Brute force O(N)
-    let currentMin = nums[0];
-    for(let i = 1; i < nums.length; i++) {
-      currentMin = Math.min(currentMin, nums[i]);
+    let left = 0;
+    let right = nums.length - 1;
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2);
+        if (nums[mid] > nums[right]) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
     }
-  return currentMin;
+    return nums[left];
 };
